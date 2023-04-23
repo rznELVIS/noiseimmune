69c299ffec73984b3ff89d77dd6ada47ff03d929
dmitriidemidov <dmitri-demidiv@yandex.ru>
2023-04-23 22:30:20 +0300

    Small refactoring

--- thershold/main.py
+++ thershold/main.py
@@ -1,16 +1,10 @@
-# from model import Model
 import sys
 from pathlib import Path
-import os
 
 sys.path.append(str(Path(__file__).parents[1].joinpath('modeling')))
 
 from model import Model
 from options import Options
-
-if __name__ == '__main__':
-    # sys.path.append(os.path.join(sys.path[0], '../../modules'))
-    print(len(sys.path))
 
 model = Model('Пороговый')
 options = Options()
